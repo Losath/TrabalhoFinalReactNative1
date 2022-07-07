e619c0731b6dd6e1ec147dd4dd08992736f2fcc1
douglasmpereira <d-moreira18@hotmail.com>
2022-07-06 22:03:46 -0300

    crud totalmente funcional

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import RotasPrivadas from './src/Routes/RotasPrivadas'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from "@react-navigation/native";
 import 'react-native-gesture-handler';
-// "rodrigo"
+
 
 const App = () => {
   
--- src/Components/componentsCRUD/modal/index.js
+++ src/Components/componentsCRUD/modal/index.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { Alert, StyleSheet, Text, Pressable, View, TextInput, ScrollView} from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { TouchableOpacity } from "react-native";
 import { Button } from "react-native";
 import CaixaInput from "./caixaInput";
 import CaixaInput2 from "./caxinhaInput2";
 
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 import Modal from "react-native-modal";
 
-const ModalList = ({toggleModal, isModalVisible, nome,
+const ModalList = ({
+  editarProduto,
+  toggleModal,
+  isModalVisible,
+  nome,
   setNome,
   custo,
   setCusto,
@@ -24,151 +38,299 @@ const ModalList = ({toggleModal, isModalVisible, nome,
   editando,
   edit,
   cancelar,
-  salvar,})=> {
+  salvar,
+}) => {
+  const adicionar = () => {
+    adicionarProduto();
+    toggleModal();
+  };
+  const salvarEdicao = ()=>{
+    salvar();
+    toggleModal();
+  };
+  const cancelaInput = ()=>{
+    cancelar();
+    toggleModal();
+  }
 
-    const adicionar =()=>{
-      adicionarProduto()
-      toggleModal()
+  const handleCategoria = () => {
+    switch (produto.categoria.idCategoria) {
+      case 1:
+        return <>Smartphones</>;
+      case 2:
+        return <>Tablets</>;
+      case 3:
+        return <>Monitores</>;
+      case 4:
+        return <>Mouses</>;
+      case 5:
+        return <>Teclados</>;
+      default:
+        return "Sem Categoria";
     }
+  };
 
-    return (
+  const data = [
+    { label: "Smartphones", value: "1" },
+    { label: "Tablets", value: "2" },
+    { label: "Monitores", value: "3" },
+    { label: "Mouses", value: "4" },
+    { label: "Teclados", value: "5" },
+  ];
 
-      <View style={{ flex: 1 }}>
-        <Modal style={styles.modalView} isVisible={isModalVisible}>
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Modal style={styles.modalView} isVisible={isModalVisible}>
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 30, marginBottom: 15, textAlign: "center" }}>
+            Detalhes do Item:
+          </Text>
           <View style={{ flex: 1 }}>
-            <Text style={{fontSize: 30, marginBottom: 15,textAlign: "center"}}>Detalhes do Item:</Text>
-                <View style={{ flex: 1 }}>
-                      <View >
-                      <View>
-                                  <Text style = {styles2.titulo}>{"Nome"}</Text>
-                                  <TextInput style={styles2.input} placeholder={"Iphone XR"}  value={nome} onChangeText={text => setNome(text)} />
-                      </View>
-                      <View>
-                                  <Text style = {styles2.titulo}>{"Descrição"}</Text>
-                                  <TextInput style={styles2.input} placeholder={"Smartphone Apple"}  value={descricao} onChangeText={text => setDescricao(text)} />
-                      </View>
-                      </View>
-                      <View style={{flexDirection:"row"}}>
-                          <View>
-                                <Text style = {styles3.titulo}>{"Custo"}</Text>
-                                <TextInput style={styles3.input} placeholder={"R$ 7.000"}  value={custo} onChangeText={text => setCusto(text)} />
-                          </View>
-                          {/* <View>
-                                <Text style = {styles3.titulo}>{"Categoria"}</Text>
-                                <TextInput style={styles3.input} placeholder={"Smartphones"}  value={categoria} onChangeText={text => setCategoria(text)} />
-                          </View> */}
-                      </View>
-                      <View style={{flexDirection:"row"}}>
-                          <View>
-                                <Text style = {styles3.titulo}>{"Preço Unit"}</Text>
-                                <TextInput style={styles3.input} placeholder={"R$ 7.000"}  value={preco} onChangeText={text => setPreco(text)} />
-                          </View>
-                          <View>
-                                <Text style = {styles3.titulo}>{"Quantidade"}</Text>
-                                <TextInput style={styles3.input} placeholder={"5"}  value={quantidade} onChangeText={text => setQuantidade(text)} />
-                          </View>
-                      </View>
-                </View>
-           <View style={{  flexDirection: "row", justifyContent: "flex-end"}}>
-              <TouchableOpacity onPress={adicionar} style={[styles.botoes, {backgroundColor: "green"}]}>
-                <Text>Salvar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={toggleModal} style={[styles.botoes, {backgroundColor: "red"}]}>
-                <Text>Cancelar</Text>
-              </TouchableOpacity>
+            <View>
+              <View>
+                <Text style={styles2.titulo}>{"Nome"}</Text>
+                <TextInput
+                  style={styles2.input}
+                  placeholder={"Iphone XR"}
+                  value={nome}
+                  onChangeText={(text) => setNome(text)}
+                />
               </View>
+              <View>
+                <Text style={styles2.titulo}>{"Descrição"}</Text>
+                <TextInput
+                  style={styles2.input}
+                  placeholder={"Smartphone Apple"}
+                  value={descricao}
+                  onChangeText={(text) => setDescricao(text)}
+                />
+              </View>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <View>
+                <Text style={styles3.titulo}>{"Custo"}</Text>
+                <TextInput
+                  style={styles3.input}
+                  placeholder={"R$ 7.000"}
+                  value={custo}
+                  onChangeText={(text) => setCusto(text)}
+                />
+              </View>
+              <View>
+                <View style={styles4.container}>
+                  <Dropdown
+                    style={[
+                      styles4.dropdown,
+                      isFocus && { borderColor: "blue" },
+                    ]}
+                    placeholderStyle={styles4.placeholderStyle}
+                    selectedTextStyle={styles4.selectedTextStyle}
+                    inputSearchStyle={styles4.inputSearchStyle}
+                    iconStyle={styles4.iconStyle}
+                    data={data}
+                    search
+                    maxHeight={300}
+                    labelField="label"
+                    valueField="value"
+                    placeholder={!isFocus ? "Categoria" : "..."}
+                    searchPlaceholder="Search..."
+                    value={categoria.idCategoria}
+                    onFocus={() => setIsFocus(true)}
+                    onBlur={() => setIsFocus(false)}
+                    onChange={(item) => {
+                      setCategoria({ idCategoria: item.value });
+                      setIsFocus(false);
+                    }}
+                  />
+                </View>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <View>
+                <Text style={styles3.titulo}>{"Preço Unit"}</Text>
+                <TextInput
+                  style={styles3.input}
+                  placeholder={"R$ 7.000"}
+                  value={preco}
+                  onChangeText={(text) => setPreco(text)}
+                />
+              </View>
+              <View>
+                <Text style={styles3.titulo}>{"Quantidade"}</Text>
+                <TextInput
+                  style={styles3.input}
+                  placeholder={"5"}
+                  value={quantidade}
+                  onChangeText={(text) => setQuantidade(text)}
+                />
+              </View>
+            </View>
           </View>
-        </Modal>
-    </View>
-  )
-}
-    
-    const styles = StyleSheet.create({
-      centeredView: {
-        marginTop: 200,
-        justifyContent: 'center',
-        alignItems: "center",
-      },
-      modalView: {
-        margin: 10,
-        backgroundColor: "white",
-        borderRadius: 20,
-        padding: 10,
-        alignItems: "center",
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5
-      },
-      button: {
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2
-      },
-      buttonOpen: {
-        backgroundColor: "#F194FF",
-      },
-      buttonClose: {
-        backgroundColor: "#2196F3",
-      },
-      textStyle: {
-        color: "white",
-        fontWeight: "bold",
-        textAlign: "center"
-      },
-      modalText: {
-        marginBottom: 15,
-        textAlign: "center"
-      },
-      input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-      },
-      botoes: {
-        justifyContent: "center",
-        alignItems:"center",
-        width: 80,
-        height: 40,
-        margin: 12,
-        borderRadius: 10
-
-      }
-    });
-    const styles2 = StyleSheet.create({
-      input: {
-          height: 40,
-          margin: 12,
-          borderWidth: 1,
-          padding: 10,
-          width: 340
-        },
-        titulo: {
-          fontSize: 20,
-          marginStart: 12,
-          marginBottom: -10,
           
-        }
-      });
-      const styles3 = StyleSheet.create({
-        input: {
-            height: 40,
-            margin: 12,
-            borderWidth: 1,
-            padding: 10,
-            width: 160
-          },
-          titulo: {
-            fontSize: 20,
-            marginStart: 12,
-            marginBottom: -10,
-            
-          }
-        });
+      {editando.edit ? (
+          <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
+            <TouchableOpacity
+              onPress={salvarEdicao}
+              style={[styles.botoes, { backgroundColor: "green" }]}
+            >
+              <Text>Salvar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={cancelaInput}
+              style={[styles.botoes, { backgroundColor: "red" }]}
+            >
+              <Text>Cancelar</Text>
+            </TouchableOpacity>
+            </View>
+      ):(
+            <View
+              style={{ flexDirection: "row", justifyContent: "flex-end" }}
+            >
+            <TouchableOpacity
+              onPress={adicionar}
+              style={[styles.botoes, { backgroundColor: "green" }]}
+            >
+              <Text>Adicionar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={cancelaInput}
+              style={[styles.botoes, { backgroundColor: "red" }]}
+            >
+              <Text>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
+          )}
+        </View>
+      </Modal>
+    </View>
+  );
+};
+export default ModalList;
 
-    export default ModalList;
+const styles = StyleSheet.create({
+  centeredView: {
+    marginTop: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  botoes: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 80,
+    height: 40,
+    margin: 12,
+    borderRadius: 10,
+  },
+});
+const styles2 = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 340,
+  },
+  titulo: {
+    fontSize: 20,
+    marginStart: 12,
+    marginBottom: -10,
+  },
+});
+const styles3 = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 160,
+  },
+  titulo: {
+    fontSize: 20,
+    marginStart: 12,
+    marginBottom: -10,
+  },
+});
+const styles4 = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+  },
+  dropdown: {
+    height: 45,
+    width: 152,
+    marginTop: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
